0c6d127002c769261ef7cfc55f94b0c1121a3c99
joey <46515201+fanheshe@users.noreply.github.com>
2020-02-18 10:37:07 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World vujke!");
+            Console.Read();
+        }
+    }
+}
